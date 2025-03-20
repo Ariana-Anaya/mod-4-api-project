29a6668cebaaa4994fb3f306dc85d14146a3e9d5
ariana2395 <164956347+ariana2395@users.noreply.github.com>
2025-03-19 18:17:36 -0700

    spot model files

--- aa12-authenticate-me/backend/db/models/spot.js
+++ aa12-authenticate-me/backend/db/models/spot.js
@@ -1,32 +1,141 @@
+// backend/db/models/spot.js
 'use strict';
-const {
-  Model
-} = require('sequelize');
+
+const { Model, Validator } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class Spot extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+
     static associate(models) {
       // define association here
-    }
+     //Spot belongs to User through ownerId (as Owner)
+     Spot.belongsTo(models.User, {
+      foreignKey: 'ownerId',
+      as: 'Owner',
+    });
+
+    // - Spot has many SpotImages through spotId
+    Spot.hasMany(models.SpotImage, {
+      foreignKey: 'spotId',
+      as: 'SpotImages',
+    });
+
+    //-Spot has many Reviews through spotId
+    Spot.hasMany(models.Review, {
+      foreignKey: 'spotId',
+      as: 'Reviews',
+    });
+
+    // Spot has many Bookings through spotId
+    Spot.hasMany(models.Booking, {
+      foreignKey: 'spotId',
+      as: 'Bookings',
+    });
   }
+}
+
   Spot.init({
-    ownerId: DataTypes.INTEGER,
-    address: DataTypes.STRING,
-    city: DataTypes.STRING,
-    state: DataTypes.STRING,
-    country: DataTypes.STRING,
-    lat: DataTypes.DECIMAL,
-    lng: DataTypes.DECIMAL,
-    name: DataTypes.STRING,
-    description: DataTypes.STRING,
-    price: DataTypes.DECIMAL
+    ownerId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+      
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Adress cannot be empty',
+        },
+      },
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'City cannot be empty',
+        },
+      },
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'State cannot be empty',
+        },
+      },
+    },
+    country: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Country cannot be empty',
+        },
+      },
+    },
+    lat: {
+      type: DataTypes.DECIMAL(9,6),
+      allowNull: false,
+      validate: {
+        min: -90,
+        max: 90,
+        isDecimal: {
+          msg: 'Lattitude must be valid decimal',
+        },
+      },
+    },
+    lng: {
+      type: DataTypes.DECIMAL(9,6),
+      allowNull: false,
+      validate: {
+        min: -180,
+        max: 180,
+        isDecimal: {
+          msg: 'Longitude must be valid decimal',
+        },
+      },
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 50],
+        notEmpty: {
+          msg: 'Name cannot be empty',
+        },
+      },
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Description cannot be empty',
+        },
+      },
+    },
+    price: {
+      type: DataTypes.DECIMAL(6,2),
+      allowNull: false,
+      validate: {
+        min: 1,
+        isDecimal: {
+          msg: 'Price must be valid decimal',
+        },
+      },
+    },
   }, {
     sequelize,
-    modelName: 'Spot',
-  });
+      modelName: 'Spot',
+      defaultScope: {
+        attributes: {
+          exclude: ['createdAt', 'updatedAt'],
+        },
+      },
+    }
+  );
   return Spot;
 };
--- aa12-authenticate-me/backend/db/models/spotimage.js
+++ aa12-authenticate-me/backend/db/models/spotimage.js
@@ -1,25 +1,47 @@
+// backend/db/models/spotimage.js
 'use strict';
-const {
-  Model
-} = require('sequelize');
+
+const { Model, Validator } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
-  class SpotImage extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+  class Spot extends Model {
+   
     static associate(models) {
       // define association here
+      //   - SpotImage belongs to Spot through spotId
+      SpotImage.belongsTo(models.Spot, {
+        foreignKey: 'spotId',
+        as: 'Spot',
+      });
     }
   }
   SpotImage.init({
-    spotId: DataTypes.INTEGER,
-    url: DataTypes.STRING,
-    preview: DataTypes.BOOLEAN
-  }, {
+    spotId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isUrl: {
+          msg: 'Must be in Url format',
+        }
+      }
+    },
+    preview: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+  }, 
+}, {
     sequelize,
     modelName: 'SpotImage',
-  });
+    defaultScope: {
+      attributes: {
+        exclude: ['createdAt', 'updatedAt'],
+      },
+    },
+  }
+);
   return SpotImage;
 };
