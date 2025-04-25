1a75f2a401eeacb37d24b4d5295e83096f403159
ariana2395 <164956347+ariana2395@users.noreply.github.com>
2025-04-24 17:17:41 -0700

    troubleshooting for deployment

--- aa12-authenticate-me/backend/db/migrations/20250311010935-add-first-last-name-to-users.js
+++ aa12-authenticate-me/backend/db/migrations/20250311010935-add-first-last-name-to-users.js
@@ -5,22 +5,22 @@ let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
+options.tableName = 'Users'; 
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('Users', 'firstName', {
+    await queryInterface.addColumn(options, 'firstName', {
       type: Sequelize.STRING,
       allowNull: true
-    }, options);
+    });
     
-    await queryInterface.addColumn('Users', 'lastName', {
+    await queryInterface.addColumn(options, 'lastName', {
       type: Sequelize.STRING,
       allowNull: true
-    }, options);
+    });
   },
 
   async down(queryInterface, Sequelize) {
-    options.tableName = "Users";
     await queryInterface.removeColumn(options, 'firstName');
     await queryInterface.removeColumn(options, 'lastName');
   }
