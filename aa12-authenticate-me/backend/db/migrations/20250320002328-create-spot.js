643835174ed3a2f3b3296c0ffb325d15f0278f50
ariana2395 <164956347+ariana2395@users.noreply.github.com>
2025-04-25 19:24:23 -0700

    updated spots seeder, again

--- aa12-authenticate-me/backend/db/migrations/20250320002328-create-spot.js
+++ aa12-authenticate-me/backend/db/migrations/20250320002328-create-spot.js
@@ -74,6 +74,6 @@ module.exports = {
   async down(queryInterface, Sequelize) {
     // options.tableName = "Spots";
     // await queryInterface.dropTable(options);
-    await queryInterface.dropTable("Spots");
+    await queryInterface.dropTable("Spots", options);
   }
 };
