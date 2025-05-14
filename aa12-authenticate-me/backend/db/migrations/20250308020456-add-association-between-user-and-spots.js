75c2a3b685ad6a89395c18213ce489b7e40b693b
ariana2395 <164956347+ariana2395@users.noreply.github.com>
2025-05-13 18:07:17 -0700

    user and spots

--- aa12-authenticate-me/backend/db/migrations/20250308020456-add-association-between-user-and-spots.js
+++ aa12-authenticate-me/backend/db/migrations/20250308020456-add-association-between-user-and-spots.js
@@ -9,7 +9,10 @@ if (process.env.NODE_ENV === "production") {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    const tableInfo = await queryInterface.describeTable('Spots');
+    const tableInfo = await queryInterface.describeTable({
+      tableName: 'Spots',
+      schema: options.schema
+    });
     if (!tableInfo.ownerId) {
       await queryInterface.addColumn('Spots', 'ownerId', {
         type: Sequelize.INTEGER,
